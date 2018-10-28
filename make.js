7f7de08d815f677b07c62dda70937dc8a062e595
makar <makar1227@gmail.com>
2018-10-28 13:54:06 +0300

    feat: add make function

---
+++ make.js
@@ -0,0 +1,13 @@
+function make() {
+    let arr = [];
+    let store = function() {
+
+        if (typeof arguments[0] === 'function') {
+            return arr.reduce((result, item) => arguments[0](result, item));
+        } else {
+            arr.push(...arguments);
+        }
+        return store;
+    };
+    return store(...arguments);
+}
